6548b6f3e70131933f3075816a7933bb36988bcd
Randi Sperling <randi.sperling@computacenter.com>
2020-02-29 01:10:34 -0500

    3th code upDdate

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ name = input("Enter your name: ")
 city = input("Enter where you grew up: ")
 adjective = input("Enter an adjective that best describes you: ")
 
-print("Hi! my name is " + name)
+print("Hi! I'm " + name)
 print("I grew up in " + city)
 print("The people I work with would describe me as " + adjective)
-

091bf5ac1acce4703a17670e983dff9bc8869af7
Randi Sperling <randi.sperling@computacenter.com>
2020-02-29 11:26:19 -0500

    story updates

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-name = input("What is your name? ")
+
 city = input("Where did you grow up? ")
 adjective = input("How would your friends best describe you? ")
 
-print("Hello! My name is " + name)
 print("I grew up in " + city)
 print("My friends would best describe me as " + adjective)

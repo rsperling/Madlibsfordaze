1ad187bb9412afdb6a5e7b574d1683da4f102a43
Randi Sperling <randi.sperling@computacenter.com>
2020-03-04 16:10:51 -0500

    variable updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Questionnaire:
         self.music = ""
         self.hobby = ""
         self.pet_peeve = ""
-        self.num1 = ""
+        self.desired_skill = ""
         self.book = ""
         self.bucket_list = ""
         self.adjective = ""
@@ -22,8 +22,8 @@ class Questionnaire:
         self.cocktail = input("Favorite happy hour beverage: ")
         self.music = input("What genre of music do you listen to? ")
         self.hobby = input("Favorite thing to do: ")
-        self.pet_peeve = input("What is your biggest pet peeve?: ")
-        self.num1 = input("How many pairs of shoes do you own?: ")
+        self.pet_peeve = input("What is something that really bothers you about work?: ")
+        self.desired_skill = input("One new skill you would like to learn?: ")
         self.book = input("What types of books do you like to read?: ")
         self.bucket_list = input("What is at the top of your bucket list?: ")
         self.adjective = input("One word to best describe you: ")
@@ -35,10 +35,10 @@ class Questionnaire:
         print(f"My roots are based in {self.location}")
         print(f"When I go to happy hour, I LOVE to drink {self.cocktail}")
         print(f"I love to rock out to {self.music} in the shower every morning")
-        print(f"When I am not at work, I like to {self.hobby}")
+        print(f"When I am not staring at a computer screen all day writing endless lines of code, I like to {self.hobby}")
         print(f"My coworkers may not know it, but I REALLY cannot stand {self.pet_peeve}")
-        print(f"I am a closet hoarder of approximately {self.num1} pairs of shoes")
-        print(f"When I am not reading Bocchi's novel length emails, I love to read {self.book}")
+        print(f"I am a closet hoarder of approximately {self.desired_skill} pairs of shoes")
+        print(f"When I am not reading Bocchi's email manifestos, I love to read {self.book}")
         print(f"One thing I really hope to do in the near future is {self.bucket_list}")
         print(f"Many would describe me as {self.adjective}")
 

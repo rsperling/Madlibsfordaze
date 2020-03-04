0e424fb477e4ea8c5ef0b2ddb43ae702dc0855d0
Randi Sperling <randi.sperling@computacenter.com>
2020-03-04 09:56:08 -0500

    drink to beverage

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
-name = input("What is your nombre?: ")
-location = input("Where are you from?: ")
-cocktail = input("Favorite happy hour drink: ")
-music = input("What genre of music do you listen to?: ")
-hobby = input("Favorite thing to do: ")
-pet_peeve = input("What is your biggest pet peeve?: ")
-num1 = input("How many pairs of shoes do you own?: ")
-book = input("What types of books do you like to read?: ")
-bucket_list = input("What is at the top of your bucket list?: ")
-adjective = input("One word to best describe you: ")
+import sys
 
-print("Hello! my nombre is" + name)
-print("My roots are based in " + location)
-print("When I go to happy hour, I LOVE to drink " + cocktail)
-print("I love to rock out to " + music, "in the shower every morning")
-print("When I am not at work, I like to " + hobby)
-print("My coworkers may not know it, but I REALLY cannot stand " + pet_peeve)
-print("I am a closet hoarder of approximately " + num1, "pairs of shoes")
-print("When I am not reading Bocchi's novel length emails, I love to read " + book)
-print("One thing I really hope to do in the near future is " + bucket_list)
-print("Many would describe me as" + adjective)
 
+class Questionnaire:
+    def __init__(self):
+        self.name = ""
+        self.location = ""
+        self.cocktail = ""
+        self.music = ""
+        self.hobby = ""
+        self.pet_peeve = ""
+        self.num1 = ""
+        self.book = ""
+        self.bucket_list = ""
+        self.adjective = ""
+
+    def ask_me_stuff(self):
+        print("I'm going to ask you some questions...\n")
+
+        self.name = input("What is your nombre? ")
+        self.location = input("Where are you from? ")
+        self.cocktail = input("Favorite happy hour beverage: ")
+        self.music = input("What genre of music do you listen to? ")
+        self.hobby = input("Favorite thing to do: ")
+        self.pet_peeve = input("What is your biggest pet peeve?: ")
+        self.num1 = input("How many pairs of shoes do you own?: ")
+        self.book = input("What types of books do you like to read?: ")
+        self.bucket_list = input("What is at the top of your bucket list?: ")
+        self.adjective = input("One word to best describe you: ")
+
+        print("\n\n")
+
+    def about_me(self):
+        print(f"Hello! my nombre is {self.name}")
+        print(f"My roots are based in {self.location}")
+        print(f"When I go to happy hour, I LOVE to drink {self.cocktail}")
+        print(f"I love to rock out to {self.music} in the shower every morning")
+        print(f"When I am not at work, I like to {self.hobby}")
+        print(f"My coworkers may not know it, but I REALLY cannot stand {self.pet_peeve}")
+        print(f"I am a closet hoarder of approximately {self.num1} pairs of shoes")
+        print(f"When I am not reading Bocchi's novel length emails, I love to read {self.book}")
+        print(f"One thing I really hope to do in the near future is {self.bucket_list}")
+        print(f"Many would describe me as {self.adjective}")
+
+
+if __name__ == "__main__":
+    q = Questionnaire()
+    q.ask_me_stuff()
+    q.about_me()
+    sys.exit(0)

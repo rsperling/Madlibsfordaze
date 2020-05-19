0c5551dd67f517be55411f4795d04bf2c324b59f
Randi Sperling <randi.sperling@computacenter.com>
2020-05-19 11:40:54 -0400

    updated questions

--- app.py
+++ app.py
@@ -5,39 +5,29 @@ class Questionnaire:
         self.name = ""
         self.location = ""
         self.cocktail = ""
-        self.music = ""
         self.hobby = ""
-        self.pet_peeve = ""
         self.desired_skill = ""
-        self.book = ""
         self.bucket_list = ""
         self.adjective = ""
 
     def ask_me_stuff(self):
-        print("I'm going to ask you some questions...\n")
-
+        print("Hi! I'm going to ask you some questions...\n")
         self.name = input("What is your name? ")
         self.location = input("Where are you from? ")
-        self.cocktail = input("Favorite happy hour drink: ")
-        self.music = input("What genre of music do you listen to? ")
-        self.hobby = input("Favorite thing to do: ")
-        self.pet_peeve = input("What is something that really bothers you about work?: ")
-        self.desired_skill = input("One new skill you would like to learn?: ")
-        self.book = input("What types of books do you like to read?: ")
-        self.bucket_list = input("What is at the top of your bucket list?: ")
+        self.cocktail = input("Favorite Q-life drink: ")
+        self.hobby = input("Favorite thing to do during quarantine?: ")
+        self.desired_skill = input("One new skill you have learned during Q-life?: ")
+        self.bucket_list = input("What will you do first when Q-life is over?: ")
         self.adjective = input("One word to best describe you: ")
 
         print("\n\n")
 
     def about_me(self):
         print(f"Hello! my name is {self.name}")
-        print(f"My roots are based in {self.location}")
-        print(f"When I go to happy hour, I LOVE to drink {self.cocktail}")
-        print(f"I love to rock out to {self.music} in the shower every morning")
-        print(f"When I am not staring at a computer screen all day writing endless lines of code, I like to {self.hobby}")
-        print(f"My coworkers may not know it, but I REALLY cannot stand {self.pet_peeve}")
-        print(f"I would love to learn how to {self.desired_skill} in my spare time")
-        print(f"When I am not reading Bocchi's email manifestos, I love to read {self.book}")
+        print(f"I am from {self.location}")
+        print(f"The Q-life has me drinking {self.cocktail}")
+        print(f"My favorite thing to do during the quarantine is {self.hobby}")
+        print(f"Because I have so much spare time in the Q-life, I've learned how to {self.desired_skill}")
         print(f"One thing I really hope to do in the near future is {self.bucket_list}")
         print(f"Many would describe me as {self.adjective}")
 
